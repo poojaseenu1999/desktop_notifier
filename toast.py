168b26322af56cfe487071fa4f6d25553affca7f
pooja srinivasan <58687165+poojaseenu1999@users.noreply.github.com>
2020-07-31 00:54:28 +0530

    commiting

---
+++ toast.py
@@ -0,0 +1,4 @@
+import win10toast
+toaster = win10toast.ToastNotifier()
+
+toaster.show_toast('hello! pooja srinivasan','Get ready to fire on with work!',icon_path='C:\\Users\Intel.DESKTOP-Q9LCO1R\\Desktop\\python\\desktopnotify\\fire.ico',duration=10)
